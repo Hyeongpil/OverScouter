655adf78a8e1e646c8ce8f5a42b9939339a6c704
hyeongpil <chlgudvlf@gmail.com>
2017-08-07 22:44:33 +0900

    리모트컨피그 추가 페이스북 광고공간 아이디 변경

--- app/src/main/java/com/hyeongpil/overscouter/stage/Stage_13_Activity.java
+++ app/src/main/java/com/hyeongpil/overscouter/stage/Stage_13_Activity.java
@@ -2,12 +2,17 @@ package com.hyeongpil.overscouter.stage;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.crash.FirebaseCrash;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 import com.hyeongpil.overscouter.BaseActivity;
 import com.hyeongpil.overscouter.R;
 import com.hyeongpil.overscouter.util.GlobalApplication;
@@ -28,6 +33,8 @@ public class Stage_13_Activity extends BaseActivity {
     private String tear;
     private double clearTime = 0;
     private boolean isClicked = false;
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
+    private String ad_config = "1";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,7 +43,31 @@ public class Stage_13_Activity extends BaseActivity {
         View containView = container.inflate();
         ButterKnife.bind(this);
         isClicked = false;
+        configFetch();
     }
+
+    private void configFetch(){
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder().build();
+        mFirebaseRemoteConfig.setDefaults(R.xml.remote_config_defaults);
+        long cacheExpiration = 3600; // 1 hour in seconds.
+
+        mFirebaseRemoteConfig.fetch(cacheExpiration)
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.e(TAG,"remote config success");
+                            mFirebaseRemoteConfig.activateFetched();
+                        } else {
+                            Log.e(TAG,"remote config fail");
+                        }
+                        ad_config = mFirebaseRemoteConfig.getString("ad_config");
+                        Log.e(TAG,"adconfig ="+ad_config);
+                    }
+                });
+    }
+
 
     @OnClick(R.id.tv_one)
     void oneClick(){
@@ -105,7 +136,7 @@ public class Stage_13_Activity extends BaseActivity {
     }
     private void showInterstitial() {
         if(!isClicked){
-            TedAdFront.showFrontAD(this, getString(R.string.facebook_id), getString(R.string.reword_ad_unit_id), TedAdHelper.AD_ADMOB, new OnFrontAdListener() {
+            TedAdFront.showFrontAD(this, getString(R.string.facebook_id), getString(R.string.reword_ad_unit_id), Integer.parseInt(ad_config), new OnFrontAdListener() {
                 @Override
                 public void onDismissed(int adType) {
                     tearIntent();
